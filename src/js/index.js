76c18ffdb8eab798a683427ba1214feaecb3c0f0
harish tagadghar <harishtgtg@gmail.com>
2020-03-25 23:43:52 +0530

    first ajex  call for searching recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
-// Global app controller
-import num from './test';
-const x = 99
-console.log(`imorted number from test module ${num} ${x}`);
+import axios from 'axios'; // importing axios library
+
+async function searchquery(query){ 
+    const data = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`) //axios is a npm library used to featch the data we can also use featch keyword but its not supported for all browsers
+    console.log(data.data.recipes);
+    
+}
+searchquery('pizza')
