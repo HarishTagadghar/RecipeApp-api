b515cff4e5108c68b210646018d6f96e85cbef08
harish tagadghar <harishtgtg@gmail.com>
2020-03-26 20:20:12 +0530

    modified recipe title text

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -8,6 +8,21 @@ export let clearInput = () => { //exporting the function which clears the input
 export let clearlist = () => { // exporting the funciong which clears the recipies list
     dom.resultList.innerHTML = '';
 }
+const limitTitle = (title , limit = 17) => {
+    const newtitle = [];
+    if(title.length > limit){
+    title.split(' ').reduce((acc,cur) => {
+        if(acc + cur.length <= limit){
+            newtitle.push(cur)
+        }
+        return acc + cur.length
+    },0)
+    return `${newtitle.join(' ')} ...`
+    }else{
+    return title
+
+    }
+}
 
 const renderRecipe = (recipe) => { // a function which takes  a single recipe list item and add it to hardcoded html 
     let markup = ` 
@@ -17,7 +32,7 @@ const renderRecipe = (recipe) => { // a function which takes  a single recipe li
                             <img src="${recipe.image_url}" alt="Test">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
