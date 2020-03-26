194609b821e6b5bbf8eacb16a7b9c815464bf319
harish tagadghar <harishtgtg@gmail.com>
2020-03-26 21:07:23 +0530

    added loader on result list

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import  { dom } from './view/dom';
+import  { dom ,renderLoader , clearLoader } from './view/dom';
 import Search from './models/search';
 import * as searchView from './view/searchView'
 
@@ -15,11 +15,13 @@ let controleSearch = async () => {
     // 3) prepair ui for result
     searchView.clearInput();
     searchView.clearlist(); 
+    renderLoader(dom.result)
 
     // 4) search for recipes
     await state.search.getRecipe() // calling method getresult stored in the constructor search we are using await because the getrecipe method is stored in a async function
     // 5) desplay recipes on ui
     searchView.recipes(state.search.recipes) //colling a funcion recipes of searchView module and the argument is the recipes objects
+    clearLoader()
 }
 }
 dom.searchForm.addEventListener('submit', e => {
--- src/js/view/dom.js
+++ src/js/view/dom.js
@@ -1,5 +1,23 @@
 export let dom =  {
     searchForm: document.querySelector('.search'),
     inputValue: document.querySelector('.search__field'),
-    resultList: document.querySelector('.results__list')
+    resultList: document.querySelector('.results__list'),
+    result: document.querySelector('.results')
 };
+
+export const renderLoader = parentElement => {
+    const loader = `
+    <div class="loader">
+    <svg>
+    <use href="img/icons.svg#icon-cw">
+    </svg>
+    </div>
+    `
+    parentElement.insertAdjacentHTML('afterbegin',loader)
+}
+export const  clearLoader = () => {
+    const loader = document.querySelector('.loader');
+    if(loader) {
+        loader.parentElement.removeChild(loader)
+    } 
+}
